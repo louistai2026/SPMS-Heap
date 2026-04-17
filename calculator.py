f72bd1cdeca3a98996435897a9b022b630328212
louistai2026 <257933358+louistai2026@users.noreply.github.com>
2026-04-17 11:17:34 +0800

    Delete calculator.py

--- calculator.py
+++
@@ -1,5 +0,0 @@
-class ScoreCalculator:
-    def calculate(self, student, course):
-        # Polymorphic call to Student.calculate()
-        student.calculate(course)
-        return student.overall
